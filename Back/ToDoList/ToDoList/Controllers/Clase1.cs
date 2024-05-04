f145e12e4ff59301bf97afd65098b6269faa8040
Agencias-Way <way_odoo@agenciasway.com>
2024-05-04 12:39:32 -0600

    create_API

---
+++ Back/ToDoList/ToDoList/Controllers/Clase1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    [Route("[controller]")]
+    public class Clase1 : Controller
+    {
+        #region  * tipo de datos*
+        // GET: Clase1
+        [HttpGet]
+        public String Index()
+        {
+            string strVar;
+            int intVar;
+            bool blnVar;
+            long longVar;
+            float floatVar;
+            double doubleVar;
+            decimal decimalVar;
+            char charVar;
+
+            strVar = "cadena de caracteres";
+            intVar = 1;
+            blnVar = true;
+            longVar = 0;
+            floatVar = 0;
+            doubleVar = 0.1;
+            decimalVar = 123;
+            charVar = 'v';
+
+
+            return strVar;
+        }
+
+        #endregion
+
+
+        #region *Despues*
+        // GET: Clase1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Clase1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clase1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clase1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clase1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clase1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clase1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+    #endregion
+}
--- Back/ToDoList/ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ Back/ToDoList/ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd216c935521e5f06fca7260a7994828b0143f05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac81f891ef77cdbefee75c2fd899bfaeb91cd984")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
